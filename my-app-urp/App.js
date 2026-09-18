e46b40da592c45c54e6ce96838a8fac8431eb512
Diego-Espinoza-URP <202220911@urp.edu.pe>
2026-09-18 22:29:16 +0000

    avance 1 lab01

--- my-app-urp/App.js
+++ my-app-urp/App.js
@@ -1,20 +1,22 @@
+import React from 'react';
+import { StyleSheet, View, SafeAreaView, Platform, StatusBar as RNStatusBar } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.pantalla}>
+        <StatusBar style="dark" />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pantalla: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f4f5fb',
+    paddingTop: Platform.OS === 'android' ? RNStatusBar.currentHeight : 0,
   },
 });
